8ab92a7a2487aad8e6bcc8e4f3133b0457277ea5
KYJ <nananan1213@gmail.com>
2026-02-01 15:23:28 +0900

    chore: git init

---
+++ src/main/java/kyj/area/teamprofile/TeamProfileApplication.java
@@ -0,0 +1,13 @@
+package kyj.area.teamprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProfileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kyj/area/teamprofile/TeamProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package kyj.area.teamprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
